ae42853605a89ce012fd32d79aa1195fb2f75cfc
mrkirby153 <mr.austinwhyte@gmail.com>
2016-12-04 19:23:11 -0800

    Add utility to fech UUIDs from Mojang's servers

---
+++ Core/src/test/me/mrkirby153/utils/UUIDFetcherTest.java
@@ -0,0 +1,28 @@
+package me.mrkirby153.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.UUID;
+
+public class UUIDFetcherTest {
+    private static UUID NOTCH_UUID = java.util.UUID.fromString("069a79f4-44e9-4726-a5be-fca90e38aaf5");
+    private static UUID UUID = java.util.UUID.fromString("8c8785dd-f067-41e1-ad19-7559c842ca45");
+    private static byte[] UUID_BYTES = {-116, -121, -123, -35, -16, 103, 65, -31, -83, 25, 117, 89, -56, 66, -54, 69};
+
+    @Test
+    public void fromBytes() throws Exception {
+        Assert.assertEquals(UUID, UUIDFetcher.fromBytes(UUID_BYTES));
+    }
+
+    @Test
+    public void getUUIDOf() throws Exception {
+        Assert.assertEquals(NOTCH_UUID, UUIDFetcher.getUUIDOf("Notch"));
+    }
+
+    @Test
+    public void toBytes() throws Exception {
+        Assert.assertArrayEquals(UUID_BYTES, UUIDFetcher.toBytes(UUID));
+    }
+
+}
