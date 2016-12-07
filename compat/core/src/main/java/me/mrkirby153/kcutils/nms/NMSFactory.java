9d0483e38354b31c06f2b7bbfba3541e8cc8f51c
mrkirby153 <mrkirby153@users.noreply.github.com>
2016-12-07 10:42:32 -0800

    Remove debug statements

--- compat/core/src/main/java/me/mrkirby153/kcutils/nms/NMSFactory.java
+++ compat/core/src/main/java/me/mrkirby153/kcutils/nms/NMSFactory.java
@@ -23,12 +23,11 @@ public class NMSFactory {
             plugin.getLogger().info(String.format("Attempting to load class %s", className));
             try {
                 NMS nms = (NMS) Class.forName(className).newInstance();
-                plugin.getLogger().info(String.format("Using compatibility class %s implementing me.mrkirby153.kcutils.nms.NMS version %s", className, nms.getNMSVersion()));
+                plugin.getLogger().info(String.format("Using compatibility class %s implementing NMS version %s", className, nms.getNMSVersion()));
                 nms.enable(plugin);
                 return nms;
             } catch (Throwable t) {
                 // Ignore
-                t.printStackTrace();
             }
         }
         plugin.getLogger().warning("Could not find a compatibility class!");
