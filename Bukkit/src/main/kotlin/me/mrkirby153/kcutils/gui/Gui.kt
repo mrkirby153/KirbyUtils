4a9e08c11b33b9008f9d1dc8b49c05e15a729ba1
mrkirby153 <mr.austinwhyte@gmail.com>
2017-11-27 18:14:04 -0800

    Add support for multiple observers

--- Bukkit/src/main/kotlin/me/mrkirby153/kcutils/gui/Gui.kt
+++ Bukkit/src/main/kotlin/me/mrkirby153/kcutils/gui/Gui.kt
@@ -12,9 +12,11 @@ import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import java.lang.ref.WeakReference
 import java.util.*
 import java.util.function.BiConsumer
 import java.util.function.Consumer
+import kotlin.collections.ArrayList
 
 /**
  * An abstract class of a gui
@@ -24,10 +26,17 @@ import java.util.function.Consumer
 abstract class Gui<T : JavaPlugin>(protected var plugin: T, rows: Int, title: String) : Listener {
 
     private var actions: MutableMap<Int, BiConsumer<Player, ClickType>> = HashMap()
-    var inventory: Inventory? = null
-        private set
+    protected val inventory: Inventory = Bukkit.createInventory(null, rows * 9)
+
+    var listenerRegistered = false
 
     private val onOpenEvents = ArrayList<Consumer<Player>>()
+    private val onCloseEvents = ArrayList<Consumer<Player>>()
+
+    /**
+     * A list of players who currently have the GUI open
+     */
+    private val observers = mutableListOf<WeakReference<Player>>()
 
     /**
      * Adds a button (clickable item) to the GUI
@@ -38,7 +47,7 @@ abstract class Gui<T : JavaPlugin>(protected var plugin: T, rows: Int, title: St
      */
     fun addButton(slot: Int, item: ItemStack, action: BiConsumer<Player, ClickType>) {
         actions.put(slot, action)
-        inventory!!.setItem(slot, item)
+        inventory.setItem(slot, item)
     }
 
     abstract fun build()
@@ -48,16 +57,29 @@ abstract class Gui<T : JavaPlugin>(protected var plugin: T, rows: Int, title: St
      */
     fun clear() {
         this.actions.clear()
-        this.inventory!!.clear()
+        this.inventory.clear()
     }
 
-    fun inventoryClose() {
-        HandlerList.unregisterAll(this)
-        onClose()
+    private fun inventoryClose(player: Player) {
+        onCloseEvents.forEach { it.accept(player) }
+        observers.removeIf {
+            val p = it.get()
+            p != null && p == player
+        }
+        // Unregister the listener only if there are no more observers
+        if (observers.none { it.get() != null }) {
+            HandlerList.unregisterAll(this)
+            listenerRegistered = false
+        }
     }
 
+    @Deprecated("")
     fun onClose() {
 
+    }
+
+    fun onClose(consumer: Consumer<Player>) {
+        this.onCloseEvents.add(consumer)
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -80,7 +102,7 @@ abstract class Gui<T : JavaPlugin>(protected var plugin: T, rows: Int, title: St
     @EventHandler(ignoreCancelled = true)
     fun onInventoryClose(event: InventoryCloseEvent) {
         if (event.inventory == this.inventory)
-            inventoryClose()
+            inventoryClose(event.player as Player)
     }
 
     @Deprecated("")
@@ -104,6 +126,10 @@ abstract class Gui<T : JavaPlugin>(protected var plugin: T, rows: Int, title: St
      */
     fun open(player: Player) {
         onOpenEvents.forEach { e -> e.accept(player) }
+        if(!listenerRegistered){
+            Bukkit.getServer().pluginManager.registerEvents(this, plugin)
+            listenerRegistered = true
+        }
         build()
         player.openInventory(inventory)
     }
@@ -117,6 +143,11 @@ abstract class Gui<T : JavaPlugin>(protected var plugin: T, rows: Int, title: St
     }
 
     /**
+     * Gets a list of players observing the inventory
+     */
+    fun getObservers(): List<Player> = this.observers.filter { it.get() != null }.map { it.get()!! }
+
+    /**
      * Gets the [Action][BiConsumer] in a slot
      *
      * @param slot The slot
@@ -124,9 +155,5 @@ abstract class Gui<T : JavaPlugin>(protected var plugin: T, rows: Int, title: St
      */
     private fun getAction(slot: Int): BiConsumer<Player, ClickType>? {
         return actions[slot]
-    }
-
-    init {
-        this.inventory = Bukkit.createInventory(null, rows * 9, title)
     }
 }
