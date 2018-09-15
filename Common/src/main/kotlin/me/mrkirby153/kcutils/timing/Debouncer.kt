3aad08e660f7814bfb3b2a3e66b456e7d489d5b3
mrkirby153 <mr.austinwhyte@gmail.com>
2018-09-15 14:56:38 -0700

    Add debouncer and throttler

---
+++ Common/src/main/kotlin/me/mrkirby153/kcutils/timing/Debouncer.kt
@@ -0,0 +1,95 @@
+package me.mrkirby153.kcutils.timing
+
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
+
+class Debouncer<T>(private val runnable: Consumer<T>, threadFactory: ThreadFactory? = null,
+                   private val mode: Mode = Mode.TRAILING) {
+
+    private val scheduler: ScheduledExecutorService
+    private val delayedMap = ConcurrentHashMap<T, Future<*>>()
+
+    private var nextRun = 0L
+
+    init {
+        if (threadFactory != null) {
+            scheduler = Executors.newSingleThreadScheduledExecutor(threadFactory)
+        } else {
+            scheduler = Executors.newSingleThreadScheduledExecutor()
+        }
+    }
+
+    /**
+     * Triggers the debounce with the given delay
+     *
+     * @param key The key
+     * @param delay The delay
+     * @param unit The units for the delay
+     */
+    fun debounce(key: T, delay: Long, unit: TimeUnit) {
+        if (mode == Mode.TRAILING) {
+            val prev = delayedMap.put(key, scheduler.schedule({
+                try {
+                    runnable.accept(key)
+                } finally {
+                    delayedMap.remove(key)
+                }
+            }, delay, unit))
+            prev?.cancel(true)
+        }
+        if (mode == Mode.LEADING) {
+            if (System.currentTimeMillis() > nextRun) {
+                try {
+                    runnable.accept(key)
+                } finally {
+                    nextRun = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(delay,
+                            unit)
+                }
+            }
+        }
+        if (mode == Mode.BOTH) {
+            if (System.currentTimeMillis() > nextRun) {
+                try {
+                    runnable.accept(key)
+                } finally {
+                    nextRun = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(delay,
+                            unit)
+                }
+            } else {
+                val prev = delayedMap.put(key, scheduler.schedule({
+                    try {
+                        runnable.accept(key)
+                    } finally {
+                        delayedMap.remove(key)
+                    }
+                }, nextRun - System.currentTimeMillis(), TimeUnit.MILLISECONDS))
+                prev?.cancel(true)
+            }
+        }
+    }
+
+    /**
+     * Shuts down the debouncer
+     */
+    fun shutdown() {
+        scheduler.shutdownNow()
+    }
+
+    fun hasPending(): Boolean {
+        if (mode == Mode.TRAILING || mode == Mode.BOTH) {
+            return delayedMap.size > 0
+        }
+        return false
+    }
+
+    enum class Mode {
+        LEADING,
+        TRAILING,
+        BOTH
+    }
+}
---
+++ Common/src/main/kotlin/me/mrkirby153/kcutils/timing/Throttler.kt
@@ -0,0 +1,46 @@
+package me.mrkirby153.kcutils.timing
+
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
+
+/**
+ * Throttles execution of an event
+ */
+class Throttler<T>(private val event: Consumer<T>) {
+
+    private val executed = mutableMapOf<T, Long>()
+
+    /**
+     * Triggers throttled execution of the event
+     *
+     * @param key The key to pass into the event
+     * @param time The throttle time
+     * @param unit The time units
+     */
+    fun trigger(key: T, time: Long, unit: TimeUnit) {
+        val next = executed[key]
+        if (next != null) {
+            if (System.currentTimeMillis() < next) {
+                return // Discard the event because it's throttled
+            }
+        }
+        val runNext = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(time, unit)
+        executed[key] = runNext
+        event.accept(key)
+    }
+
+    /**
+     * Checks if the function is throttled for the given key
+     *
+     * @param key The key
+     *
+     * @return True if the key's execution is throttled
+     */
+    fun throttled(key: T): Boolean {
+        val next = executed[key]
+        if (next != null) {
+            return System.currentTimeMillis() < next
+        }
+        return false
+    }
+}
