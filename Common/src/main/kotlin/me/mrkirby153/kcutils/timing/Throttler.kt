3ef81f4c582fa99ea311673acecc8a1b9297b564
mrkirby153 <mr.austinwhyte@gmail.com>
2019-10-29 20:38:48 -0700

    Enhance throttler
    Suport using a concurrent backing map as well as adding reset and update
    functions for manipulating timeouts

--- Common/src/main/kotlin/me/mrkirby153/kcutils/timing/Throttler.kt
+++ Common/src/main/kotlin/me/mrkirby153/kcutils/timing/Throttler.kt
@@ -1,14 +1,17 @@
 package me.mrkirby153.kcutils.timing
 
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
 import java.util.function.Consumer
 
 /**
  * Throttles execution of an event. Subsequent calls will be dropped until the quiet period has passed.
+ *
+ * A concurrent backing map can be used by passing `concurrent` into the constructor
  */
-class Throttler<T>(private val event: Consumer<T?>?) {
+class Throttler<T> @JvmOverloads constructor(private val event: Consumer<T?>?, concurrent: Boolean = false) {
 
-    private val executed = mutableMapOf<T?, Long>()
+    private val executed = if (concurrent) ConcurrentHashMap() else mutableMapOf<T?, Long>()
 
     /**
      * Triggers throttled execution of the event
@@ -54,5 +57,26 @@ class Throttler<T>(private val event: Consumer<T?>?) {
     fun getTimeRemaining(key: T): Long {
         val time = executed[key] ?: 0
         return time - System.currentTimeMillis()
+    }
+
+    /**
+     * Resets the throttler for a given key
+     *
+     * @param key The key
+     */
+    fun reset(key: T) {
+        executed.remove(key)
+    }
+
+    /**
+     * Updates the time remaining in the throttler **without triggering it**
+     *
+     * @param key The key
+     * @param duration The new duration
+     * @param timeUnit The new time unit
+     */
+    fun update(key: T, duration: Long, timeUnit: TimeUnit) {
+        executed[key] = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(duration,
+                timeUnit)
     }
 }
