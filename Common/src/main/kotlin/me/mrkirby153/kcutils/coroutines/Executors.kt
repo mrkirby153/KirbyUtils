e61c4abe61b489c3f98c3d8bf8d036dac07e2170
mrkirby153 <mr.austinwhyte@gmail.com>
2023-03-22 18:03:15 -0700

    Cache coroutine scopes

--- Common/src/main/kotlin/me/mrkirby153/kcutils/coroutines/Executors.kt
+++ Common/src/main/kotlin/me/mrkirby153/kcutils/coroutines/Executors.kt
@@ -1,19 +1,33 @@
 package me.mrkirby153.kcutils.coroutines
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.future.asCompletableFuture
+import java.util.Collections
+import java.util.WeakHashMap
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executor
 import java.util.concurrent.ForkJoinPool
+import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
+
+private val scopeCache =
+    Collections.synchronizedMap(WeakHashMap<CoroutineContext, CoroutineScope>())
+
+/**
+ * Gets or creates a new coroutine scope for the given [dispatcher]
+ */
+fun getOrCreateScope(dispatcher: CoroutineContext) = scopeCache.computeIfAbsent(dispatcher) {
+    CoroutineScope(dispatcher + SupervisorJob())
+}
 
 /**
  * Run the given coroutine asynchronously and return a [CompletableFuture] of its result
  */
 inline fun <T> Executor.runAsync(crossinline body: suspend CoroutineScope.() -> T): CompletableFuture<T> {
-    return CoroutineScope(this.asCoroutineDispatcher() + EmptyCoroutineContext).async {
+    return getOrCreateScope(this.asCoroutineDispatcher() + EmptyCoroutineContext).async {
         body()
     }.asCompletableFuture()
 }
