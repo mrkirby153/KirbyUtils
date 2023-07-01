ba930649bc12ad3dce17864f6e344a0001654af1
mrkirby153 <mr.austinwhyte@gmail.com>
2023-07-01 13:13:19 -0700

    Publish to github pages

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.0")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
 }
 
