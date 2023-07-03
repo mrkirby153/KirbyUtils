1a97090677e96adcc087911ee7d8059af25630d4
mrkirby153 <mr.austinwhyte@gmail.com>
2023-07-03 14:55:23 -0700

    Version coroutines jdk8 support lib

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
 }
