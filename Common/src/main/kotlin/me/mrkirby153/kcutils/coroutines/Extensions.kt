122648291f38cb3949f1f9ac43a5d1ebe8a4bb80
mrkirby153 <mr.austinwhyte@gmail.com>
2023-09-24 19:32:00 -0700

    Deprecate CompletableFuture#await

--- Common/src/main/kotlin/me/mrkirby153/kcutils/coroutines/Extensions.kt
+++ Common/src/main/kotlin/me/mrkirby153/kcutils/coroutines/Extensions.kt
@@ -1,19 +1,13 @@
 package me.mrkirby153.kcutils.coroutines
 
-import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.future.await
 import java.util.concurrent.CompletableFuture
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
 
 /**
  * Awaits the result of the given [CompletableFuture]
  */
-suspend fun <T> CompletableFuture<T>.await() = suspendCancellableCoroutine<T> {
-    it.invokeOnCancellation { cancel(true) }
-    whenComplete { r, e ->
-        when {
-            e != null -> it.resumeWithException(e)
-            else -> it.resume(r)
-        }
-    }
-}
+@Deprecated(
+    "Use CompletableFuture.await()",
+    ReplaceWith("this.await()", "kotlinx.coroutines.future.await")
+)
+suspend fun <T> CompletableFuture<T>.await(): T = this.await()
